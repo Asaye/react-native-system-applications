1a028afd704a4f63cf67717d5462772c60d12595
Asaye Dilbo <32922135+Asaye@users.noreply.github.com>
2019-08-26 23:58:55 +0300

    Add files via upload

--- android/src/main/java/com/sysapps/SysappsPackage.java
+++ android/src/main/java/com/sysapps/SysappsPackage.java
@@ -1,18 +1,17 @@
 package com.sysapps;
 
 import com.sysapps.volume.Volume;
- import com.sysapps.files.Files;
- import com.sysapps.wifi.Wifi;
- import com.sysapps.bluetooth.Bluetooth;
- import com.sysapps.brightness.Brightness;
- import com.sysapps.alarm.Alarm;
- import com.sysapps.alarm.NotificationEvent;
- import com.sysapps.calls.PhoneCalls;
- import com.sysapps.contacts.PhoneContacts;
+import com.sysapps.files.Files;
+import com.sysapps.wifi.Wifi;
+import com.sysapps.bluetooth.Bluetooth;
+import com.sysapps.brightness.Brightness;
+import com.sysapps.alarm.Alarm;
+import com.sysapps.calls.PhoneCalls;
+import com.sysapps.contacts.PhoneContacts;
 import com.sysapps.sms.Sms;
- import com.sysapps.image.ImageCapturer;
- import com.sysapps.audio.AudioRecorder;
- import com.sysapps.video.VideoRecorder;
+import com.sysapps.image.ImageCapturer;
+import com.sysapps.audio.AudioRecorder;
+import com.sysapps.video.VideoRecorder;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
@@ -36,18 +35,17 @@ public class SysappsPackage implements ReactPackage {
     List<NativeModule> modules = new ArrayList<>();
 
     modules.add(new Volume(reactContext));
-     modules.add(new Files(reactContext));
-     modules.add(new Wifi(reactContext));
-     modules.add(new Bluetooth(reactContext));
-     modules.add(new Brightness(reactContext));
-     modules.add(new Alarm(reactContext));
-     modules.add(new NotificationEvent(reactContext));
+    modules.add(new Files(reactContext));
+    modules.add(new Wifi(reactContext));
+    modules.add(new Bluetooth(reactContext));
+    modules.add(new Brightness(reactContext));
+    modules.add(new Alarm(reactContext));
     modules.add(new PhoneCalls(reactContext));
-     modules.add(new Sms(reactContext));
-     modules.add(new PhoneContacts(reactContext));
-     modules.add(new ImageCapturer(reactContext));
-     modules.add(new AudioRecorder(reactContext));
-     modules.add(new VideoRecorder(reactContext));
+    modules.add(new Sms(reactContext));
+    modules.add(new PhoneContacts(reactContext));
+    modules.add(new ImageCapturer(reactContext));
+    modules.add(new AudioRecorder(reactContext));
+    modules.add(new VideoRecorder(reactContext));
 
     return modules;
   }
